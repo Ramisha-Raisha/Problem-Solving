48d659efe5162a7269c2b48912c7a8903fc5f904
Ramisha-Raisha <raisharamishamustafa@gmail.com>
2022-09-15 21:18:42 +0600

    10th Problem

--- Hacker Rank/C/10. Student marks sum.c
+++ Hacker Rank/C/10. Student marks sum.c
@@ -1,3 +1,24 @@
+
+/*You are given an array of integers, marks, denoting the marks scored by students in a class.
+The alternating elements marks0, marks2, marks4, and so on denote the marks of boys.
+Similarly, marks1, marks3, marks5, and so on denote the marks of girls.
+The array name marks, works as a pointer which stores the base address of that array. In other words, marks contains the address where marks0 is stored in the memory.
+For example, let marks = [3,2,5] and marks stores 0x7fff9575c05f. Then, 0x7fff9575c05f is the memory address of marks0.
+Complete the function, marks_summation(int* marks, char gender, int number_of_students) which returns the total sum of:
+marks of boys if gender = b
+marks of girl if gender = g
+The locked code stub reads the elements of marks along with gender. Then, it calls the function marks_summation(marks, gender, number_of_students) to get the sum of alternate elements as explained above and then prints the sum.
+Input Format
+>>The first line contains number_of_students, denoting the number of students in the class, hence the number of elements in marks.
+>>Each of the number_of_students subsequent lines contains marksi.
+>>The next line contains gender.
+Constraints
+ #number_of_students <=10^3
+ #1<=marksi<=10^3 (where 0<= i< number_of_students)
+ #gender = g or b
+ ####Output Format
+  The output should contain the sum of all the aternate elements in marks as explained above.*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
